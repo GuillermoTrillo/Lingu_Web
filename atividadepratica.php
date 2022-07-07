1fa348d12819440914b7bdbce532cca4d2afd23b
PacoTercero <106634146+PacoTercero@users.noreply.github.com>
2022-07-06 21:11:50 -0300

    Add files via upload

---
+++ atividadepratica.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Aluno {
+    Matricula = "a";
+    Cod_Curso = 1;
+    Cod_Professor = 1;
+    Nome = "aaaaaaaa";
+    Data_De_Nascimento = "10/10/10";
+}
+class Professor {
+    Cod_Professor = 1;
+    nome = "aa";
+}
+class Curso {
+    Cod_Curso = 1;
+    Cod_Professor = 1;
+    Descricao = "a";
+    Quantidades_alunos = 1;
+}
+
+?>
