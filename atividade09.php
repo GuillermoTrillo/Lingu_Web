8c92ff4798bcbe9be20172b1dca038a3647724c8
PacoTercero <106634146+PacoTercero@users.noreply.github.com>
2022-07-06 21:22:52 -0300

    Add files via upload

---
+++ atividade09.php
@@ -0,0 +1,17 @@
+<?php
+
+class aeronave {
+
+    codAeronave = 1;
+    nomeModelO¡o = "NOME";
+    numeroDeAssentos = 3;
+}
+class TrechoAeronave {
+
+ numeroTrecho = 1;
+  codigoAeroporto = 10;
+   dataPartida = "10/10/10";
+     horaPartida = "10:10";
+     horaChegada = "10/10/10";
+}
+?>
